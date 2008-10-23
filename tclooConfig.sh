f4835b33e31440a9f91adaf19f965816d98395a1
kennykb <kennykb>
2008-10-23 15:56:58 +0000

    Added a dummy tclooConfig.sh to restore 8.6 buildability

---
+++ tclooConfig.sh
@@ -0,0 +1,10 @@
+# This is a dummy tclooConfig.sh file to fill in empty options
+# when building against Tcl 8.6, which has tcloo in the coroe
+
+TCLOO_LIB_SPEC=""
+TCLOO_STUB_LIB_SPEC=""
+TCLOO_INCLUDE_SPEC=""
+# The next should copy TCL_PRIVATE_INCLUDE_SPEC, but I don't use it anyway.
+TCLOO_PRIVATE_INCLUDE_SPEC=""
+TCLOO_CFLAGS=-DUSE_TCLOO_STUBS
+TCLOO_VERSION="8.6"
